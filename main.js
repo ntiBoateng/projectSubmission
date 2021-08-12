272162390bc30e6d7b169147d1778dedfc81bf1f
ntiBoateng <87483815+ntiBoateng@users.noreply.github.com>
2021-08-12 10:45:42 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
 const prompt = require('prompt-sync')({sigint: true});
-let impt = require('./calculator.js')
+let Calculator = require('./calculator.js')
 
-let dd = prompt('Do you want to add or product ?')
+let dd = prompt('Do you want to add or product ? ')
 if(dd ==='add'){
-    let x = process.argv[2]
-    let y = process.argv[2]
-    let adds = new Calculator(Number(x),Number(y))
+    let x = prompt('Enter your first number ')
+    x=Number(x)
+    let y = prompt('Enter your second number ')
+    y = Number(y)
+    let adds = new Calculator(x,y)
     let ans = adds.add
     console.log('The result is '+ans)
 }else if(dd === 'product'){
-    let x = process.argv[2]
-    let y = process.argv[2]
-    let products = new Calculator(Number(x),Number(y))
+    let x = prompt('Enter your first number ')
+    x=Number(x)
+    let y = prompt('Enter your second number ')
+    y = Number(y)
+    let products = new Calculator(x,y)
     let ans = products.product
     console.log('The result is '+ans)
 }
